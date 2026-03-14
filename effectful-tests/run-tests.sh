066fe99b3e84e8a417b2f54b561f8d8d96ef1dca
Gilbert Ramirez <gram@alumni.rice.edu>
2026-03-14 11:23:05 -0500

    Fix run-tests.sh to call development compiler

--- effectful-tests/run-tests.sh
+++ effectful-tests/run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
+COMPILER_DIR=$(realpath ../../compiler)
+export GREN_BIN="$COMPILER_DIR"/gren
+node "$COMPILER_DIR"/app make Main --output=run-tests
 
-gren make Main --output=run-tests
 node run-tests
