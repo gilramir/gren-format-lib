cdf973d1699a3b2b3ab582205ece251840617f6b
Gilbert Ramirez <gram@alumni.rice.edu>
2026-05-02 11:40:25 -0500

    Bring up to date with latest compiler-common code

--- effectful-tests/run-tests.sh
+++ effectful-tests/run-tests.sh
@@ -3,6 +3,6 @@
 set -e
 COMPILER_DIR=$(realpath ../../compiler)
 export GREN_BIN="$COMPILER_DIR"/gren
-node "$COMPILER_DIR"/app make Main --output=run-tests
+node "$COMPILER_DIR"/app make Main --output=app
 
-node run-tests
+node app
